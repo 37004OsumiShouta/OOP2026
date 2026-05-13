c3ab54bb8d1ecf4be29bf508dcfe01f88d40160e
大隅翔太 <oibc37004@ojs.ac.jp>
2026-05-13 11:16:46 +0900

    問題2-2完成

---
+++ chapter02/演習問題/Convarter/Convarter/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Convarter {
+    internal class Program {
+        static void Main(string[] args) {
+        }
+        public class InchConverter {
+            public static readonly double ratio = 0.0254;    //定数
+                                                             // メートルからフィートを求める
+            public static double Toinch(int meter) {
+                return meter / ratio;
+            }
+            // フィートからメートルを求める
+            public static double FromMeter(int inch) {
+                return inch * ratio;
+            }
+        }
+    }
+}
--- chapter02/演習問題/Exercise02/Exercise02/Program.cs
+++ chapter02/演習問題/Exercise02/Exercise02/Program.cs
@@ -1,32 +1,17 @@
 ﻿namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-
-            if (int.TryParse(args[1], out var start) && int.TryParse(args[2], out var stop)) {
-                if (args.Length >= 1 && args[0] == "-tom") {
-                    PrintInchToMeterList(int.Parse(args[1]), int.Parse(args[2]));
-                } else if (args.Length >= 1 && args[0] == "-tof") {
-                    PrintMeterToInchList(int.Parse(args[1]), int.Parse(args[2]));
-                } else {
-                    Console.WriteLine("引数エラー");
-                }
-
-                static void PrintInchToMeterList(int start, int stop) {
-                    for (int inch = start; inch <= stop; inch++) {
-                        double meter = InchConverter.ToFeet(inch);
-                        Console.WriteLine($"{inch}ft = {meter:0.0000}m");
-                    }
-                }
-                static void PrintMeterToInchList(int start, int stop) {
-                    for (int meter = start; meter <= stop; meter++) {
-                        double inch = InchConverter.FromMeter(meter);
-                        Console.WriteLine($"{meter}m = {inch:0.0000}ft");
-                    }
-                }
-            }
+            PrintMeterToInchList(1, 10); 
         }
-        public double ToFeet(int inch) {
-            return inch * 0.3048;
+                static void PrintMeterToInchList(int start ,int stop ) {
+                    for (int inch = start; inch <= stop; inch++) {
+                        double meter = ToMeter(inch);
+                        Console.WriteLine($"{inch}inch = {meter:0.0000}m");
+                    }
+                }
+           
+        private static double ToMeter(double inch) {
+            return inch * 0.0254;
         }
     }
 }
