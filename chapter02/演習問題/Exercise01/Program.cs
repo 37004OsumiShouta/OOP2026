32dde0612fd5298b7ed83b2b13a232cd0a572b90
大隅翔太 <oibc37004@ojs.ac.jp>
2026-05-13 10:18:52 +0900

    データ入力対応

--- chapter02/演習問題/Exercise01/Program.cs
+++ chapter02/演習問題/Exercise01/Program.cs
@@ -1,21 +1,46 @@
 ﻿
+
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
-            // 2.1.3
-            var songs = new Song[] {
-              new Song("Let it be", "The Beatles ", 243),
-              new Song("Bridge Over Troubled Water", "Simon & Garfunkel ", 293),
-              new Song("Close To You", "Carpenters ", 276),
-              new Song("Honesty", "Billy Joel ", 231),
-              new Song("I Will Always Love You", "Whitney Houston ", 273),
-            };
-            PrintSongs(songs);
+            var songs = new List<Song>();
+            while (true) {
+                Console.WriteLine("アーティスト名:");
+                string? artistName = Console.ReadLine();
+                if (artistName == "end") {
+                    break;
+                } else {
+                    Console.WriteLine("曲名:");
+                    string? title = Console.ReadLine();
+
+                    Console.WriteLine("演奏時間:");
+                    int length = int.Parse(Console.ReadLine());
+
+                    Song song = new Song(title, artistName, length);
+
+
+                    songs.Add(song);
+
+
+
+                    // 2.1.3
+                    //var songs = new Song[] {
+                    //new Song("Let it be", "The Beatles ", 243),
+                    //new Song("Bridge Over Troubled Water", "Simon & Garfunkel ", 293),
+                    //new Song("Close To You", "Carpenters ", 276),
+                    //new Song("Honesty", "Billy Joel ", 231),
+                    //new Song("I Will Always Love You", "Whitney Houston ", 273),
+                    PrintSongs(songs);
+                }
+            }
         }
 
-        private static void PrintSongs(Song[] songs) {
-            foreach (var obj in songs) {
-                Console.WriteLine($"{obj.Title}{obj.ArtistName}{obj.Length / 60 + ":" +  obj.Length % 60}");
+
+
+
+        private static void PrintSongs(List<Song> songs) {
+            foreach (var song in songs) {
+                Console.WriteLine($"{song.Title}{song.ArtistName}{(song.Length / 60 + ":" + song.Length % 60):00}");
             }
         }
     }
