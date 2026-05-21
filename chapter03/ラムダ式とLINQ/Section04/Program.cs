ddbd891a78c7ae611a18c2624b383cd407c32e6b
大隅翔太 <oibc37004@ojs.ac.jp>
2026-05-21 10:01:52 +0900

    演習問題3.1完成

---
+++ chapter03/ラムダ式とLINQ/Section04/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Section04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
+            var query = cities.Where(s => s.Length <= 5).ToList();
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------");
+
+            cities[0] = "Osaka";
+            foreach(var item in query) {
+                Console.WriteLine(item);
+            }
+            //IEnumerable<string> query = cities
+            //.Where(s => s.Length <= 5)
+            //.Select(s => s.ToLower());
+            //foreach (string s in query) {
+                //Console.WriteLine(s);
+            }
+        }
+    }
