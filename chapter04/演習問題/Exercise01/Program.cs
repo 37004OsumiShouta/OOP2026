d296752b08a064076a8afc4e0308b7b6d583644f
大隅翔太 <oibc37004@ojs.ac.jp>
2026-05-27 11:46:30 +0900

    問題4.1.3完成

--- chapter04/演習問題/Exercise01/Program.cs
+++ chapter04/演習問題/Exercise01/Program.cs
@@ -1,16 +1,19 @@
 ﻿
+using System.ComponentModel.Design;
+
 namespace Exercise01 {
     internal class Program {
+
         static void Main(string[] args) {
             List<string> langs = [
                "C#", "Java", "Ruby", "PHP", "Python", "TypeScript",
                 "JavaScript", "Swift", "Go",
             ];
-
+            Console.WriteLine("--4-1-1--");
             Exercise1(langs);
-            Console.WriteLine("---");
+            Console.WriteLine("--4-1-2--");
             Exercise2(langs);
-            Console.WriteLine("---");
+            
             Exercise3(langs);
             Console.WriteLine("---");
         }
@@ -18,10 +21,10 @@ namespace Exercise01 {
         private static void Exercise1(List<string> langs) {
             //foreach文
             Console.WriteLine("foreach文で出力");
-            var exists = langs.Where(s => s.Contains("S"));
-            foreach (var s in exists)
+            foreach (var s in langs) {
+                if(s.Contains("S"))
                 Console.WriteLine(s);
-
+            }
             Console.WriteLine();//改行
             //for文
             Console.WriteLine("\nfor文で出力");
@@ -30,22 +33,40 @@ namespace Exercise01 {
                     Console.WriteLine(langs[i]);
                 }
             }
+
             Console.WriteLine("\nwhile文で出力");
             int j = 0;
-            while(j < langs.Count) {
-                if (langs[j].Contains("S")) {
+            while (j < langs.Count) {
+                if (langs[j].Contains("S"))
                     Console.WriteLine(langs[j]);
-                }
                 j++;
             }
         }
 
         private static void Exercise2(List<string> langs) {
-
+            var selected = langs.Where(s => s.Contains("S"));
+            foreach (var lang in selected) {
+                Console.WriteLine(lang);
+            }
         }
 
         private static void Exercise3(List<string> langs) {
+            Console.WriteLine("\n---4-1-3---");
+            var name = langs.Find(s => s.Length == 10);
+           
+            var message = GetMessage(name) ?? "unknown";
+            Console.WriteLine(message);
 
+            message = null;
+            message ??= "null";
+        }
+
+        private static object? DefaultMessage() {
+            return DefaultMessage;
+        }
+
+        private static object? GetMessage(string code) {
+            return code;
         }
     }
 }
