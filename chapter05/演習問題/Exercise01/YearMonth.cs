789e89c4ec61656d6c2dbb8b766c2e7f312efc23
大隅翔太 <oibc37004@ojs.ac.jp>
2026-06-01 11:18:27 +0900

    問題5.1.2完成

--- chapter05/演習問題/Exercise01/YearMonth.cs
+++ chapter05/演習問題/Exercise01/YearMonth.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise01 {
     public class YearMonth {
+        //5.1.1
         public int Year { get; init; }
 
         public int Month { get; init; }
+        //5.1.2
+        public bool Is21Century => 2001 <= Year && 2100 >= Year;
+        //5.1.3
+        public YearMonth AddOneMonth() {
 
-        public YearMonth(int year,int month) {
+        }
+
+        public YearMonth(int year, int month) {
             Year = year;
             Month = month;
         }
