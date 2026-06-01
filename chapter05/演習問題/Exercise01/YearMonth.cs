06d9f5b209dd88c13bdd1a1aa38463b60ff6a911
大隅翔太 <oibc37004@ojs.ac.jp>
2026-06-01 11:59:50 +0900

    問題3.1.3完成

--- chapter05/演習問題/Exercise01/YearMonth.cs
+++ chapter05/演習問題/Exercise01/YearMonth.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -15,8 +16,14 @@ namespace Exercise01 {
         public bool Is21Century => 2001 <= Year && 2100 >= Year;
         //5.1.3
         public YearMonth AddOneMonth() {
-
+            if (Month == 12) {
+                return new YearMonth(Year + 1, 1);
+            } else {
+                return new YearMonth(Year, Month + 1);
+            }
         }
+        //5.1.4
+
 
         public YearMonth(int year, int month) {
             Year = year;
---
+++ chapter05/演習問題/Exercise02/Exercise02/YearMonth.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise02 {
+    internal class YearMonth {
+        private int v1;
+        private int v2;
+
+        public YearMonth(int v1, int v2) {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+    }
+}
