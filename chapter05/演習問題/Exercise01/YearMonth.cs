f681b87156e10014021f0b5f5fd5ad51875c4e2f
大隅翔太 <oibc37004@ojs.ac.jp>
2026-06-04 09:25:00 +0900

    5.1.3~5.1.4完成

--- chapter05/演習問題/Exercise01/YearMonth.cs
+++ chapter05/演習問題/Exercise01/YearMonth.cs
@@ -23,6 +23,7 @@ namespace Exercise01 {
             }
         }
         //5.1.4
+        public override string ToString() => Year + "年" + Month + "月"; 
 
 
         public YearMonth(int year, int month) {
