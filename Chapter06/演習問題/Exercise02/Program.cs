f8b24cff1301c8a1fbaff82b02c9e1e4a2f370fa
大隅翔太 <oibc37004@ojs.ac.jp>
2026-06-10 10:29:55 +0900

    問題6.2完成

---
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("整数文字列：");
+            if (int.TryParse(Console.ReadLine(), out var number)) {
+                var num = number.ToString("#,0");
+                Console.WriteLine(num);
+            } else {
+                Console.WriteLine("変換できません");
+            }
+        }
+    }
+}
