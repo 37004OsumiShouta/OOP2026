a6973bad52bb317de5d6531b76770d02b772bd79
大隅翔太 <oibc37004@ojs.ac.jp>
2026-06-17 10:34:34 +0900

    修正

--- Chapter06/演習問題/Exercise04/Exercise04/Program.cs
+++ Chapter06/演習問題/Exercise04/Exercise04/Program.cs
@@ -2,12 +2,15 @@
     internal class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            var name = line.Split(';', '=').ToArray();
-            for (int i = 0; i < name.Length; i++) {
-                Console.WriteLine(ToJapanese(name[i])+ ":" + name[i + 1]);
-                i++;
+            foreach (var item in line.Split(';')) {
+            var word = item.Split('=');
+            Console.WriteLine($"{ToJapanese(word[0])}:{word[1]}");
+            //var name = line.Split(';', '=').ToArray();
+            //for (int i = 0; i < name.Length; i++) {
+            //    Console.WriteLine(ToJapanese(name[i]) + ":" + name[i + 1]);
+            //    i++;
             }
-            //foreach (var names in name) {
+
         }
         static string ToJapanese(string key) {
             return key switch {
