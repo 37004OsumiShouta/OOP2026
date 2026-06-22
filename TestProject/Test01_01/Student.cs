905cb5b6dac80c4cd2bd2d8d0ce18863e87ba1c4
大隅翔太 <oibc37004@ojs.ac.jp>
2026-06-22 11:25:45 +0900

    設問１_Studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,6 +1,12 @@
-﻿namespace Test01_01 {
+﻿using System.ComponentModel;
+
+namespace Test01_01 {
     public class Student {
-        
+        public string Name { get; set; } = string.Empty;
+
+        public string Subject { get; set; } = string.Empty;
+
+        public int Score { get; set; }
 
 
     }
