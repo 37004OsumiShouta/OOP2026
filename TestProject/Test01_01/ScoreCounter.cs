a63bf09e2aae1503b0edc1468ca13997d1da7ca1
大隅翔太 <oibc37004@ojs.ac.jp>
2026-06-29 00:55:47 +0900

    テスト再提出1_01

--- TestProject/Test01_01/ScoreCounter.cs
+++ TestProject/Test01_01/ScoreCounter.cs
@@ -1,45 +1,63 @@
-﻿namespace Test01_01 {
-    public class ScoreCounter {
+﻿using System.ComponentModel.Design;
+
+namespace Test01_01
+{
+    public class ScoreCounter
+    {
         private IEnumerable<Student> _score;
 
         // コンストラクタ
-        public ScoreCounter(string filePath) {
+        public ScoreCounter(string filePath)
+        {
             _score = ReadScore(filePath);
         }
 
         //メソッドの概要：
-        private static IEnumerable<Student> ReadScore(string filePath) {
-            var sales = new List<Student>();
-            var lines = File.ReadAllLines(filePath);
-            foreach(string line in lines) {
+        private static IEnumerable<Student> ReadScore(string filePath)
+        {
+            List<Student> scores = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
                 string[] items = line.Split(',');
-                Student sale = new Student {
+
+                Student student = new Student
+                {
                     Name = items[0],
                     Subject = items[1],
                     Score = int.Parse(items[2])
                 };
-                sales.Add(sale);
+
+                scores.Add(student);
             }
-
-
-
-
-
-
-
-            return sales;
+            return scores;
         }
 
         //メソッドの概要：
-        public IDictionary<string, int> GetPerStudentScore() {
+        public IDictionary<string, int> GetPerStudentScore()
+        {
             var dict = new Dictionary<string, int>();
+            foreach (var student in _score)
+            {
+                if (dict.ContainsKey(student.Subject))
+                {
+                    dict[student.Subject] += student.Score;
+                }
+                else
+                {
+                    dict[student.Subject] = student.Score;
+                }
 
 
 
 
 
 
-            return dict;
+               
+            }
+           return dict;
         }
+
     }
+  
 }
