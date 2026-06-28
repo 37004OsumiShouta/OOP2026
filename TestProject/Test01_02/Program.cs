b1cd551c3753d81e8dce51b82d946836608e492a
大隅翔太 <oibc37004@ojs.ac.jp>
2026-06-29 00:25:39 +0900

    テスト再提出

--- TestProject/Test01_02/Program.cs
+++ TestProject/Test01_02/Program.cs
@@ -62,47 +62,44 @@ namespace Test01_02 {
 
         //問題１　合計値を表示（式形式で記述せよ）
         //　　　　出力結果【618】
-        private static void Exercise01(int[] numbers) {
-            var number = numbers.Sum(n => n + n);
-            foreach (var n in numbers)
-                Console.WriteLine(n);
-        }
+        private static void Exercise01(int[] numbers) => Console.WriteLine(numbers.Sum());
 
 
         //問題２　偶数の最大値を表示（式形式で記述せよ）
         //　　　　出力結果【94】
-        private static void Exercise02(int[] numbers) {
+        private static void Exercise02(int[] numbers) => Console.WriteLine(numbers.Where(n => n % 2 == 0).Max());
             //var max = 
 
-        }
+        
 
 
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
         private static void Exercise03(int[] numbers) {
-           
+            foreach (var n in numbers.OrderBy(n => n))
+            {
+                Console.WriteLine(n + " ");
+            }
         }
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(int[] numbers) {
-            var query = numbers
-                .Where(n => n >= 10 && n > 50);
-                Console.WriteLine(query);
-            
-        }
+            foreach (var n in numbers.Where(n => n >= 10 && n <= 50))
+                Console.WriteLine(n + " ");
+            }
 
         //問題５　Countメソッドを使い、小文字の'n'が含まれている都市名がいくつあるかカウントして結果を表示
         //　　　　出力結果【5】
         private static void Exercise05(List<string> cities) {
-      
+            Console.WriteLine(cities.Count(city => city.Contains("n")));
         
         }
 
         //問題６　全都市数
         //　　　　出力結果【8】
         private static void Exercise06(List<string> cities) {
-        
+            Console.WriteLine(cities.Count);
         }
 
         //問題７　各都市名をアルファベット順（昇順）に出力
@@ -116,7 +113,10 @@ namespace Test01_02 {
         //          Paris
         //          Tokyo】
         private static void Exercise07(List<string> cities) {
-        
+        foreach(var city in cities.OrderBy(c => c))
+            {
+                Console.WriteLine(city);
+            }
 
         }
 
@@ -131,6 +131,10 @@ namespace Test01_02 {
         //　　　　  Canberra : 8文字
         //　　　　  Hong Kong : 9文字】
         private static void Exercise08(List<string> cities) {
+            foreach(var city in cities)
+            {
+                Console.WriteLine($"{city} : {city.Length}文字");
+            }
     
         
         }
@@ -146,7 +150,10 @@ namespace Test01_02 {
         //          New Delhi : 9文字
         //          Hong Kong : 9文字】
         private static void Exercise09(List<string> cities) {
-        
+        foreach(var city in cities.OrderBy(c => c.Length))
+            {
+                Console.WriteLine($"{city} : {city.Length}文字");
+            }
         
         
         }
@@ -156,8 +163,10 @@ namespace Test01_02 {
         //        【London
         //          Berlin】
         private static void Exercise10(List<string> cities) {
-        
-
+        foreach(var city in cities.Where(c => c.Length == 6))
+            {
+                Console.WriteLine(city);
+            }
         }
     }
 }
