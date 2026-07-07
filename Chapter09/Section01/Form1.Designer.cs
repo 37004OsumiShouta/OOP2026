35a017fa68361e0c3e4f691ad0c56a9aa9bedfe3
大隅翔太 <oibc37004@ojs.ac.jp>
2026-07-07 14:18:52 +0900

    曜日の出力

--- Chapter09/Section01/Form1.Designer.cs
+++ Chapter09/Section01/Form1.Designer.cs
@@ -35,6 +35,12 @@
             label3 = new Label();
             label4 = new Label();
             btBirthCalc = new Button();
+            tbOut3 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
             ((System.ComponentModel.ISupportInitialize)nudDay).BeginInit();
             SuspendLayout();
             // 
@@ -140,17 +146,77 @@
             btBirthCalc.UseVisualStyleBackColor = true;
             btBirthCalc.Click += btBirthCalc_Click_1;
             // 
+            // tbOut3
+            // 
+            tbOut3.Location = new Point(608, 362);
+            tbOut3.Multiline = true;
+            tbOut3.Name = "tbOut3";
+            tbOut3.Size = new Size(447, 160);
+            tbOut3.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(608, 226);
+            label5.Name = "label5";
+            label5.Size = new Size(110, 32);
+            label5.TabIndex = 4;
+            label5.Text = "経過日数";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(608, 226);
+            label6.Name = "label6";
+            label6.Size = new Size(110, 32);
+            label6.TabIndex = 4;
+            label6.Text = "経過日数";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(608, 226);
+            label7.Name = "label7";
+            label7.Size = new Size(110, 32);
+            label7.TabIndex = 4;
+            label7.Text = "経過日数";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(608, 226);
+            label8.Name = "label8";
+            label8.Size = new Size(110, 32);
+            label8.TabIndex = 4;
+            label8.Text = "経過日数";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(608, 327);
+            label9.Name = "label9";
+            label9.Size = new Size(49, 32);
+            label9.TabIndex = 4;
+            label9.Text = "メモ";
+            label9.Click += label9_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(2004, 1250);
             Controls.Add(btBirthCalc);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(nudDay);
+            Controls.Add(tbOut3);
             Controls.Add(tbOut2);
             Controls.Add(tbOut);
             Controls.Add(btGet);
@@ -178,5 +244,11 @@
         private Label label3;
         private Label label4;
         private Button btBirthCalc;
+        private TextBox tbOut3;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
     }
 }
--- Chapter09/Section01/Form1.cs
+++ Chapter09/Section01/Form1.cs
@@ -9,26 +9,41 @@ namespace Section01 {
         private void btGet_Click(object sender, EventArgs e) {
             DateTime date = dtpDate.Value;
             tbOut.Text = date.AddDays((double)nudDay.Value).ToString();
+
         }
         private void btBirthCalc_Click_1(object sender, EventArgs e) {
             DateTime birth = dtpBirth.Value;
             DateTime today = DateTime.Today;
             int age = today.Year - birth.Year;
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+
+            var dayOfWeek = culture.DateTimeFormat.GetDayName(birth.DayOfWeek);
             if (today < birth.AddYears(age)) {
                 age--;
             }
-            tbOut.Text = $"あなたは{GetAge(birth,today)}歳です";
+            tbOut.Text = $"あなたは{age}歳です";
             TimeSpan ts = today.Date - birth.Date;
             tbOut2.Text = $"生まれてから{ts.Days}日目です。";
+            tbOut3.Text = $"生まれた{birth.Month}月{birth.Day}日は第{NthWeek(birth)}週の{dayOfWeek}です。";
         }
-        //年齢を求めるメソッド
-        static int GetAge(DateTime birthday, DateTime targetDay) {
-            var age = targetDay.Year - birthday.Year;
-            if (targetDay < birthday.AddYears(age)) {
-                age--;
-                
-            }
-            return age;
+        ////年齢を求めるメソッド
+        //static int GetAge(DateTime birthday, DateTime targetDay) {
+        //    var age = targetDay.Year - birthday.Year;
+        //    if (targetDay < birthday.AddYears(age)) {
+        //        age--;
+
+        //    }
+        //    return age;
+
+        static int NthWeek(DateTime date) {
+            var firstDay = new DateTime(date.Year, date.Month, 1);
+            var firstDayOfWeek = (int)(firstDay.DayOfWeek);
+            return (date.Day + firstDayOfWeek - 1) / 7 + 1;
+        }
+
+        private void label9_Click(object sender, EventArgs e) {
+
         }
     }
 }
