72b747a5b2291f73e23c7b2329e75e98d46ccef3
大隅翔太 <oibc37004@ojs.ac.jp>
2026-07-09 15:01:32 +0900

    演習問題9.1完成

--- Chapter09/演習問題/Exercise01/Form1.cs
+++ Chapter09/演習問題/Exercise01/Form1.cs
@@ -1,7 +1,29 @@
+using System.Configuration;
+using System.Globalization;
+
 namespace Exercise01 {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+
+        private void btButton1_Click(object sender, EventArgs e) {
+            DateTime date = DateTime.Now;
+            tbOut1.Text = date.ToString("yyyy/MM/dd HH:ss");
+
+        }
+        private void btButton2_Click(object sender, EventArgs e) {
+            DateTime date = DateTime.Now;
+            var s1 = date.ToString("yyyy”NMMŒŽdd“ú HHŽžmm•ªss•b");
+            tbOut2.Text = s1;
+        }
+
+        private void btButton3_Click(object sender, EventArgs e) {
+            DateTime date = DateTime.Now;
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var str = date.ToString("gg y”N MŒŽ d“ú(dddd)", culture);
+            tbOut3.Text = str;
         }
     }
 }
